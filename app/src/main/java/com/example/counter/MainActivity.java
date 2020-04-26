5b7c4e7837414079a1efd3ba5fc3a71fbf060f09
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-04-26 15:28:20 +0200

    Added increment button, layout not adjusted yet

---
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.counter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void increment(View view)
+    {
+        counter++;
+        TextView textView = (TextView) findViewById(R.id.counter_number);
+        String counterRepresentant = String.valueOf(counter);
+        textView.setText(counterRepresentant);
+    }
+}
